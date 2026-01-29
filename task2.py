a10160d53e8953928bc265664ff9b53bb1916160
dassuraj997-afk <dassuraj997@gmail.com>
2026-01-29 20:52:13 +0530

    initail comit

---
+++ task2.py
@@ -0,0 +1,16 @@
+#Takes a user's first name and last name as input.
+first_name=input("Enter first name: ")
+last_name=input("Enter last name: ")
+
+#Concatenates the first name and last name into a full name.
+full_name=first_name+' ' + last_name
+
+
+#Prints a personalized greeting message using the full name
+
+print("Hello, ",full_name + " Welcome to the python program. ")
+
+
+
+
+
