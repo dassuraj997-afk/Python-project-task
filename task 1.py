66b921b98e1bd8fdeae57b619e25a7c7f8336f94
dassuraj997-afk <dassuraj997@gmail.com>
2026-01-30 19:17:09 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,26 @@
+#Takes two numbers as input from the user.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performs  mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+#result
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+
+
+
+
+
+
+
+
+
+
